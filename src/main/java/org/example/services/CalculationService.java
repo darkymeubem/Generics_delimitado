bd4945c61cb32b55482a4f89717d94bee53fcc86
Felipe Pedroza <felipelp2910@gmail.com>
2024-10-02 13:24:13 -0300

    Implementando a logica de Generics ao metódo max

---
+++ src/main/java/org/example/services/CalculationService.java
@@ -0,0 +1,20 @@
+package org.example.services;
+
+import java.util.List;
+
+public class CalculationService {
+    //Fazendo o metódo receber um tipo "T" qualquer, extendendo da interface Comparable seus metódos, assim
+    //dando continuidade ao código
+    public static <T extends Comparable<T>> T max(List<T> list) {
+        if (list.isEmpty()) {
+            throw new IllegalStateException("List can't be empty");
+        }
+        T max = list.get(0);
+        for (T item : list) {
+            if (item.compareTo(max) > 0) {
+                max = item;
+            }
+        }
+        return max;
+    }
+}
